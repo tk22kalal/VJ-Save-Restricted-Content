1a153a7e02a9600b72b5cd76109420829a6b30e5
Tarang Kalal <127711733+tk22kalal@users.noreply.github.com>
2025-10-18 17:41:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,8 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION, LOGIN_SYSTEM
 
 if STRING_SESSION is not None and LOGIN_SYSTEM == False:
-	TechVJUser = Client("TechVJ" ,api_id=api_id, api_hash=api_hash, session_string=STRING_SESSION)
-	TechVJUser.start()
+        TechVJUser = Client("TechVJ" ,api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
 else:
     TechVJUser = None
 
@@ -28,10 +23,13 @@ class Bot(Client):
     async def start(self):
             
         await super().start()
+        if TechVJUser is not None:
+            await TechVJUser.start()
         print('Bot Started Powered By @VJ_Botz')
 
     async def stop(self, *args):
-
+        if TechVJUser is not None:
+            await TechVJUser.stop()
         await super().stop()
         print('Bot Stopped Bye')
 
