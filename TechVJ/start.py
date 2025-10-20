ea0e3ff72af78b8794e8888d1bfa8ce649c03c14
Tarang Kalal <127711733+tk22kalal@users.noreply.github.com>
2025-10-20 14:03:11 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -1,7 +1,6 @@
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
 import os
 import asyncio
 import pyrogram
