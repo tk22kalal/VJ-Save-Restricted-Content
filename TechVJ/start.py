7f92081d7cca1b7578e75a62b3d807a7dcffc1f1
Tarang Kalal <127711733+tk22kalal@users.noreply.github.com>
2025-11-06 01:39:42 +0530

    Create start.py

---
+++ TechVJ/start.py
@@ -0,0 +1,15 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+# This file serves as the main entry point for all handlers
+# The code has been refactored into separate modules for better readability
+
+# Import all command handlers
+from TechVJ.commands import send_start, send_help, send_cancel
+
+# Import batch download handler with error tracking
+from TechVJ.batch_handler import save
+
+# All handlers are automatically registered via decorators
+# No need for additional registration here
